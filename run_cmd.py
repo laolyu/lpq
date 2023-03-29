b9663f7d43c18de779cb0f76ab3f875e31d2415b
lvmingyao <b4hand@qq.com>
2023-03-29 13:24:45 +0800

    lpq api automation

--- run_cmd.py
+++ run_cmd.py
@@ -6,7 +6,7 @@ sys.path.append(r'C:\air')
 from pack import tk_1st
 
 # cmd = r'hrp run .\testcases\withdraw_test.yaml -g'
-cmd = r'hrp run .\testcases\withdraw_test.yaml -g -p http://localhost:8888 -c'
+cmd = r'hrp run .\testcases\withdraw_test.yaml -g -p http://localhost:8888 '
 
 os.system(cmd)
 tk_1st.show('run end')
