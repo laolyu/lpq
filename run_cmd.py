652ee4737ab19e068682c142cbde8c052e57958a
lvmingyao <b4hand@qq.com>
2023-06-24 20:32:06 +0800

    linux本地python直接执行

--- run_cmd.py
+++ run_cmd.py
@@ -1,12 +1,12 @@
 # coding:utf-8
 import os
-import sys
+# import sys
 
-sys.path.append(r'C:\air')
-from pack import tk_1st
+#sys.path.append(r'C:\air')
+#from pack import tk_1st
 
 # cmd = r'hrp run .\testcases\withdraw_test.yaml -g'
 cmd = r'hrp run .\testcases\withdraw_test.yaml -g -p http://localhost:8888 -c'
 
 os.system(cmd)
-tk_1st.show('run end')
+# tk_1st.show('run end')
