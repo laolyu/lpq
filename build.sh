77124597917cdb2761528645df6edaa470eb88ae
lvmingyao <b4hand@qq.com>
2023-06-24 22:52:54 +0800

    docker run python

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ echo "运行容器python执行自动化"
 #-w=$WORKSPACE：指定workspace
 #--volumes-from=jenkins_save01：将jenkins容器中的workspace映射到python容器中，此时jenkins中git拉下来的代码就会被映射进去
 #docker run --rm -w=$WORKSPACE --volumes-from=jenkins python:lpq
-python3 run_cmd.py
+docker run -dit --name python --network=host -v /usr/bin/python3:/usr/bin/python3 -v /usr/local/jenkins:/var/jenkins_home -w=$WORKSPACE python:lpq bash
+
+#python3 run_cmd.py
 echo "python执行自动化执行成功"
