cd64919c350247ae9109823bb32c0ee13d33252e
lvmingyao <b4hand@qq.com>
2023-07-09 14:04:48 +0800

    v1.0 test pass

--- run_cmd.py
+++ run_cmd.py
@@ -5,8 +5,8 @@ import os
 #sys.path.append(r'C:\air')
 #from pack import tk_1st
 
-# cmd = r'hrp run .\testcases\withdraw_test.yaml -g'
-cmd = r'hrp run .\testcases\withdraw_test.yaml -g -p http://localhost:8888 -c'
+cmd = r'hrp run .\testcases\ -g -c'
+# cmd = r'hrp run .\testcases\withdraw_test.yaml -g -p http://localhost:8888 -c'
 
 os.system(cmd)
 # tk_1st.show('run end')
